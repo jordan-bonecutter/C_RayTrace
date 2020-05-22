ecf80ca819d88f580bbfe623636e074aca4047c5
Jordan Bonecutter <jordanbonecutter@gmail.com>
2020-05-22 13:12:25 -0700

    also added more vector functions

--- vec3d.c
+++ vec3d.c
@@ -6,56 +6,70 @@
 
 #include "vec3d.h"
 #include <assert.h>
+#include <math.h>
 
-vec3d_t v3d_add(vec3d_t* v1, vec3d_t* v2){
+void v3d_add(vec3d_t* v1, vec3d_t* v2, vec3d_t* res){
   assert(v1);
   assert(v2);
-
-  vec3d_t ret;
+  assert(res);
 
   /* add componentwise */
-  ret.x = v1->x + v2->x;
-  ret.y = v1->y + v2->y;
-  ret.z = v1->z + v2->z;
-
-  return ret;
+  res->x = v1->x + v2->x;
+  res->y = v1->y + v2->y;
+  res->z = v1->z + v2->z;
 }
 
-vec3d_t v3d_scale(vec3d_t* v, double s){
-  assert(v);
-
-  vec3d_t ret;
-
-  /* vector scale */
-  ret.x = v->x * s;
-  ret.y = v->y * s;
-  ret.z = v->z * s;
-
-  return ret;
-}
-
-vec3d_t v3d_hadamard(vec3d_t* v1, vec3d_t* v2){
+void v3d_sub(vec3d_t* v1, vec3d_t* v2, vec3d_t* res){
   assert(v1);
   assert(v2);
+  assert(res);
 
-  vec3d_t ret;
+  /* add componentwise */
+  res->x = v1->x - v2->x;
+  res->y = v1->y - v2->y;
+  res->z = v1->z - v2->z;
+}
+
+void v3d_scale(vec3d_t* v, double s, vec3d_t* res){
+  assert(v);
+  assert(res);
+
+  /* vector scale */
+  res->x = v->x * s;
+  res->y = v->y * s;
+  res->z = v->z * s;
+}
+
+void v3d_hadamard(vec3d_t* v1, vec3d_t* v2, vec3d_t* res){
+  assert(v1);
+  assert(v2);
+  assert(res);
 
   /* hadamard product */
-  ret.x = v1->x * v2->x;
-  ret.y = v1->y * v2->y;
-  ret.z = v1->z * v2->z;
+  res->x = v1->x * v2->x;
+  res->y = v1->y * v2->y;
+  res->z = v1->z * v2->z;
+}
 
-  return ret;
+void v3d_normalize(vec3d_t* v){
+  assert(v);
+
+  double mag = v3d_mag(v);
+  v->x /= mag;
+  v->y /= mag;
+  v->z /= mag;
 }
 
 double v3d_dot(vec3d_t* v1, vec3d_t* v2){
   assert(v1);
   assert(v2);
 
-  double ret;
+  return v1->x*v2->x + v1->y*v2->y + v1->z*v2->z;
+}
 
-  ret = v1->x*v2->x + v1->y*v2->y + v1->z*v2->z;
+double v3d_mag(vec3d_t* v){
+  assert(v);
 
-  return ret;
+  return sqrt(v->x*v->x + v->y*v->y + v->z*v->z);
 }
 
--- vec3d.h
+++ vec3d.h
@@ -11,10 +11,13 @@ typedef struct{
   double x, y, z;
 }vec3d_t;
 
-vec3d_t v3d_add(vec3d_t* v1, vec3d_t* v2);
-vec3d_t v3d_scale(vec3d_t* v, double s);
-vec3d_t v3d_hadamard(vec3d_t* v1, vec3d_t* v2);
-double  v3d_dot(vec3d_t* v1, vec3d_t* v2);
+void   v3d_add(vec3d_t* v1, vec3d_t* v2, vec3d_t* res);
+void   v3d_sub(vec3d_t* v1, vec3d_t* v2, vec3d_t* res);
+void   v3d_scale(vec3d_t* v, double s, vec3d_t* res);
+void   v3d_hadamard(vec3d_t* v1, vec3d_t* v2, vec3d_t* res);
+void   v3d_normalize(vec3d_t* v);
+double v3d_dot(vec3d_t* v1, vec3d_t* v2);
+double v3d_mag(vec3d_t* v);
 
 #endif
 
